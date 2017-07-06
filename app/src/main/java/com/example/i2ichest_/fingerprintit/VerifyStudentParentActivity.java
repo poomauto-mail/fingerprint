5c632a38ad056c54409e9aa2382dea67f6262b4a
poom-automail <worachon7@gmail.com>
2017-07-06 21:30:02 +0700

    deleted space

--- app/src/main/java/com/example/i2ichest_/fingerprintit/VerifyStudentParentActivity.java
+++ app/src/main/java/com/example/i2ichest_/fingerprintit/VerifyStudentParentActivity.java
@@ -7,12 +7,11 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import com.example.i2ichest_.fingerprintit.manager.WSManager;
-import com.example.i2ichest_.fingerprintit.model.PersonModel;
+import com.example.i2ichest_.fingerprintit.model.StudentModel;
 
 public class VerifyStudentParentActivity extends AppCompatActivity {
-
+    String splitName[];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,19 +23,23 @@ public class VerifyStudentParentActivity extends AppCompatActivity {
         EditText studenId = (EditText)findViewById(R.id.studentId);
         EditText phone = (EditText)findViewById(R.id.phone);
         EditText email = (EditText)findViewById(R.id.email);
-
-
-
-
-        final ProgressDialog progress = ProgressDialog.show(VerifyStudentParentActivity.this,"Please Wait...","Please wait...",true);
         WSManager manager = WSManager.getWsManager(this);
-        PersonModel personmodel = new PersonModel();
-        manager.doVerifyStudentParent(personmodel, new WSManager.WSManagerListener() {
+        StudentModel studentModel = new StudentModel();
+        final ProgressDialog progress = ProgressDialog.show(VerifyStudentParentActivity.this,"Please Wait...","Please wait...",true);
+
+        splitName = name.getText().toString().split("");
+      /*  studentModel.getPerson().setPersonID(12L);
+        studentModel.getPerson().setFirstName(splitName[0]);
+        studentModel.getPerson().setLastName(splitName[1]);*/
+        studentModel.getStudent().setStudentID(Long.parseLong(studenId.getText().toString()));
+        studentModel.getStudent().setParentPhone(phone.getText().toString());
+
+        manager.doVerifyStudentParent(studentModel, new WSManager.WSManagerListener() {
             @Override
             public void onComplete(Object response) {
                 progress.dismiss();
                 if(response.toString()!=null){
-
+                    Log.d("student response ",response.toString());
                 }
 
 
