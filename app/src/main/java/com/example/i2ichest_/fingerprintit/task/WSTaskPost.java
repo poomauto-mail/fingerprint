2172a850411ea7604d7496304354f9802ab77505
poom-automail <worachon7@gmail.com>
2017-07-08 01:07:07 +0700

    nothing

--- app/src/main/java/com/example/i2ichest_/fingerprintit/task/WSTaskPost.java
+++ app/src/main/java/com/example/i2ichest_/fingerprintit/task/WSTaskPost.java
@@ -35,7 +35,7 @@ public class WSTaskPost extends AsyncTask<String,String,String> {
     @Override
     protected String doInBackground(String... params) {
         RequestBody body = RequestBody.create(JSON, params[1]);
-        Request request = new Request.Builder().url("http://10.0.0.97:8080".concat(params[0])).post(body).build();
+        Request request = new Request.Builder().url("http://10.0.0.106:8080".concat(params[0])).post(body).build();
         try{
             Response response = client.newCall(request).execute();
             return response.body().string();
